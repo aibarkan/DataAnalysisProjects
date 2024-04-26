5fcf4dcb372479c7d3697aeec279dacb3ae55b5d
aibarkan <65514833+aibarkan@users.noreply.github.com>
2024-04-26 13:55:28 -0400

    Update ExcelColValDataTransform.py

--- ExcelColValDataTransform.py
+++ ExcelColValDataTransform.py
@@ -3,30 +3,25 @@ import pandas as pd
 attribute_data=pd.read_excel(r"file_name")
 col_to_update = "col_name"
 export_file = r"output_filename"
-attribute_data.shape
 
+#Check data spread
+attribute_data.shape
 attribute_data[100:120]
+#set updates values for intended values, otherwise keep original value
 def update_value(row, col_name):
-    value = row[col_name].lower()
+    value = row[col_name]
     exposure_mapping = {
-        'low to mod': "Low to Moderate Exposure",
-        'light to mod': "Low to Moderate Exposure",
-        'low to high': "Low to High Exposure",
-        "moderate to heavy": "Moderate to High Exposure",
-        "moderate to high": "Moderate to High Exposure",
-        "heavy" : "High Exposure",
-        "high" : "High Exposure",
-        'no' : "No Exposure",
-        'light': 'Low Exposure',
-        'moderate':'Moderate Exposure',
-        'yes':'Some Exposure'
+        'anti-fog': "Anti-Fog",
+        'fog-free with foam strip': "Fog-Free With Foam Strip",
+        'foam band': "Foam Band",
+        'no': "No Fog Resistance",
+        'yes':'Fog Resistance'
     }
-    if value == "low":
-        return 'Low Exposure'
     for key in exposure_mapping:
-        if key in value:
+        if key in value.lower():
             return exposure_mapping[key]
-    return "N/A"
+    return value
+
 #Set col to str type 
 attribute_data[col_to_update] = attribute_data[col_to_update].astype(str)
 attribute_data[col_to_update] = attribute_data.apply(update_value, args=(col_to_update,),axis=1)
